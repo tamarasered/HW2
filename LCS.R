f99338b744362383848beb5519ebe2a1a9ab4b80
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 16:39:39 -0400

    saving LCS.R

--- LCS.R
+++ LCS.R
@@ -5,13 +5,14 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('clean_data.csv')
+clean_data <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
-LCS <- data.frame(Likelihood_Recommend_H=df$Likelihood_Recommend_H, Condition_Hotel_H=df$Condition_Hotel_H, Staff_Cared_H=df$Staff_Cared_H)
-LCS <- LCS[!is.na(LCS$Condition_Hotel_H),]
-LCS <- LCS[!is.na(LCS$Staff_Cared_H),]
+# PART F: Creating a new dataframe with ‘Likelihood_Recommend_H’, Condition_Hotel_H 
+# and Staff_Cared_H all cleaned.
+new_df_LCS <- clean_data[,clean_data$Likelihood_Recommend_H, clean_data$Condition_Hotel_H, clean_data$Staff_Cared_H)
+
 ## end your R code and logic 
 
 ####################################
