1828943381b325273aef2d4c99f87e77606b4905
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-15 16:55:53 -0400

    saving LCS.R

--- LCS.R
+++ LCS.R
@@ -9,8 +9,14 @@ df <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
+
+#Creating a new dataframe which has three columns.The first column is already clean, has no NAs 
 LCS <- data.frame(Likelihood_Recommend_H=df$Likelihood_Recommend_H, Condition_Hotel_H=df$Condition_Hotel_H, Staff_Cared_H=df$Staff_Cared_H)
+
+#Removing NA for the second column
 LCS <- LCS[!is.na(LCS$Condition_Hotel_H),]
+
+#Removing NA for the third column
 LCS <- LCS[!is.na(LCS$Staff_Cared_H),]
 ## end your R code and logic 
 
