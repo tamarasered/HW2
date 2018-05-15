ddf866ff662a4f9b9e7259dbdc24d4fd153f7012
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 15:10:35 -0400

    saving descript.R

--- descript.R
+++ descript.R
@@ -13,13 +13,18 @@ clean_data <- read.csv('clean_data.csv')
 #Crearing a vector for the likelihood to recommend column.
 LR <- c(clean_data$Likelihood_Recommend_H)
 # First find the maximum and minimum values in the column and create corresponding variables.
-maximum <- max(LR)
-minimum <- min(LR)
+Maximum <- max(LR)
+Maximum
+Minimum <- min(LR)
+Minimum
 # Having those variables the range can be found.
-range <- maximum-minimum
+Range <- maximum-minimum
+Range
 # Calculating and creating corresponding variables for mean and standard deviation.
-mean <- mean(LR)
+Mean <- mean(LR)
+Mean
 SD <- sd(LR)
+SD
 #Calculating the quantiles.
 quantile(LR, probs = c(0.25,0.75,0.9))
 #Generating the histogram.
