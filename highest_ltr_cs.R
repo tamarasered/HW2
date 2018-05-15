56a6533e5f371954c7725a0f2abeff114734fbaa
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 17:48:37 -0400

    saving highest_ltr_cs.R

--- highest_ltr_cs.R
+++ highest_ltr_cs.R
@@ -12,7 +12,8 @@ LCS <- read.csv('LCS.csv')
 # PART F.2: Output the ratings (for each of staff cared and hotel condition) 
 # where there was the most highest LTR.
 df_max_ltr <- LCS[LCS$Likelihood_Recommend_H=max(LCS$Likelihood_Recommend_H),]
-
+# PART F.3:	Output the conditions where there were the most number of survey’s 
+# completed – in terms of the rating of  the hotel condition and the staff caring.
 
 ## end your R code and logic 
 
@@ -20,7 +21,6 @@ df_max_ltr <- LCS[LCS$Likelihood_Recommend_H=max(LCS$Likelihood_Recommend_H),]
 ##### write output file ############
 write.csv(df, file = 'highest_ltr.csv')
 ####################################
-
 
 
 
