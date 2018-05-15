a93653eda51cfa8a163dcf73b3a05d9242d0f86f
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 14:21:41 -0400

    saving clean.R

--- clean.R
+++ clean.R
@@ -5,15 +5,18 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('raw_data.csv')
+raw_data <- read.csv('raw_data.csv')
 ####################################
 
 ## start writing your R code from here
+library(dplyr)
+InitialClean <- surveyData %>% filter(!is.na(Likelihood_Recommend_H))
 
+length(surveyData$Likelihood_Recommend_H) - length(InitialClean$Likelihood_Recommend_H)
 ## end your R code and logic 
 
 ####################################
 ##### write output file ############
-write.csv(df, file = 'data.csv')
+write.csv(InitialClean, file = 'clean_data.csv')
 ####################################
 
