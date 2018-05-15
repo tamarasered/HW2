ecb111597936e98b65dd8ab9e9997e338954da6a
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-15 17:29:26 -0400

    saving LCS.R

--- LCS.R
+++ LCS.R
@@ -5,23 +5,25 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-clean_data <- read.csv('clean_data.csv')
+df <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
-# PART F: Creating a new dataframe with ‘Likelihood_Recommend_H’, Condition_Hotel_H 
-# and Staff_Cared_H all cleaned.
-new_df_LCS <- data.frame("Likelihood_Recommend_H"=clean_data$Likelihood_Recommend_H,
-                         "Condition_Hotel_H"=clean_data$Condition_Hotel_H,
-                         "Staff_Cared_H"=clean_data$Staff_Cared_H)
-clean_new_df_LCS <- new_df_LCS [!is.na(new_df_LCS$Likelihood_Recommend_H),] 
-clean_new_df_LCS <- clean_new_df_LCS [!is.na(clean_new_df_LCS$Condition_Hotel_H),] 
-clean_new_df_LCS <- clean_new_df_LCS [!is.na(clean_new_df_LCS$Staff_Cared_H),] 
+
+#Creating a new dataframe which has three columns.The first column is already clean, has no NAs 
+LCS <- data.frame(Likelihood_Recommend_H=df$Likelihood_Recommend_H, Condition_Hotel_H=df$Condition_Hotel_H, Staff_Cared_H=df$Staff_Cared_H)
+
+#Removing NA for the second column
+LCS <- LCS[!is.na(LCS$Condition_Hotel_H),]
+
+#Removing NA for the third column
+LCS <- LCS[!is.na(LCS$Staff_Cared_H),]
 ## end your R code and logic 
 
 ####################################
 ##### write output file ############
-write.csv(clean_new_df_LCS, file = 'LCS.csv')
+write.csv(LCS, file = 'LCS.csv')
 ####################################
+
 
 
