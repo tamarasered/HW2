cf9b172d9cbd7412c957b84a2d7d17d11c900c5f
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 16:53:08 -0400

    saving LCS.R

--- LCS.R
+++ LCS.R
@@ -11,11 +11,12 @@ clean_data <- read.csv('clean_data.csv')
 ## start writing your R code from here
 # PART F: Creating a new dataframe with ‘Likelihood_Recommend_H’, Condition_Hotel_H 
 # and Staff_Cared_H all cleaned.
-# new_df_LCS <- clean_data[,clean_data$Likelihood_Recommend_H, clean_data$Condition_Hotel_H, clean_data$Staff_Cared_H]
 new_df_LCS <- data.frame("Likelihood_Recommend_H"=clean_data$Likelihood_Recommend_H,
                          "Condition_Hotel_H"=clean_data$Condition_Hotel_H,
                          "Staff_Cared_H"=clean_data$Staff_Cared_H)
-
+clean_new_df_LCS <- new_df_LCS [!is.na(new_df_LCS$Likelihood_Recommend_H,
+                                       new_df_LCS$Condition_Hotel_H, 
+                                       new_df_LCS$Staff_Cared_H),] 
 ## end your R code and logic 
 
 ####################################
