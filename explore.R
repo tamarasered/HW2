2a7d83eb0d26b6f54bd2e3dede2decd52a8dfe19
ksapaev <39306914+ksapaev@users.noreply.github.com>
2018-05-15 16:02:44 -0400

    saving explore.R

--- explore.R
+++ explore.R
@@ -9,7 +9,7 @@ df <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
-explore <- data.frame(df$Likelihood_Recommend_H, df$Condition_Hotel_H, df$Staff_Cared_H)
+explore <- data.frame(Likelihood_Recommend_H=df$Likelihood_Recommend_H, Condition_Hotel_H=df$Condition_Hotel_H, Staff_Cared_H=df$Staff_Cared_H)
 explore <- explore[!is.na(explore$df.Condition_Hotel_H),]
 explore <- explore[!is.na(explore$df.Staff_Cared_H),]
 ## end your R code and logic 
