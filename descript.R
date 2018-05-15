e4d13be2338e7afc227bd4088d9f3a14229afe35
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 15:06:05 -0400

    saving descript.R

--- descript.R
+++ descript.R
@@ -5,11 +5,27 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('data.csv')
+clean_data <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
-
+# Generating descriptive statistics for the column Likelihood_Recommend_H. 
+#Crearing a vector for the likelihood to recommend column.
+LR <- c(clean_data$Likelihood_Recommend_H)
+# First find the maximum and minimum values in the column and create corresponding variables.
+maximum <- max(LR)
+minimum <- min(LR)
+# Having those variables the range can be found.
+range <- maximum-minimum
+# Calculating and creating corresponding variables for mean and standard deviation.
+mean <- mean(LR)
+SD <- sd(LR)
+#Calculating the quantiles.
+quantile(LR, probs = c(0.25,0.75,0.9))
+#Generating the histogram.
+png(filename="histogram.png")
+hist(LR)
+dev.off()
 ## end your R code and logic 
 
 ####################################
