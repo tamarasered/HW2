fdf7edfdbf592965a938719665381edfe5dd1aeb
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 15:44:17 -0400

    saving NPS.R

--- NPS.R
+++ NPS.R
@@ -5,16 +5,24 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('clean_data.csv')
+clean_data <- read.csv('clean_data.csv')
 ####################################
 
 ## start writing your R code from here
+# PART D: Writing a function, given a vector of LTR, to generate an NPS.
+LTR <- c(clean_data$Likelihood_Recommend_H)
 
+NPS <- function(LTR) {
+  promoters  <- (sum(LTR > 8))/length(LTR)
+  detractors <- (sum(LTR < 7))/length(LTR)
+  nps <- promoters-detractors 
+  return(nps)
+}
+NPS(LTR)
 ## end your R code and logic 
 
 ####################################
 ##### write output file ############
-write.csv(df, file = 'NPS.csv')
 ####################################
 
 
