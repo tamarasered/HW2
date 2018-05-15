070c2b4ac24d8385bdd1ac454a6bd8d85ea49ec4
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 17:48:13 -0400

    saving highest_ltr_cs.R

--- highest_ltr_cs.R
+++ highest_ltr_cs.R
@@ -5,10 +5,14 @@ setwd(Sys.getenv('PROJECT_HOME'))
 
 ####################################
 ## write code to read input csv into data frame
-df <- read.csv('LCS.csv')
+LCS <- read.csv('LCS.csv')
 ####################################
 
 ## start writing your R code from here
+# PART F.2: Output the ratings (for each of staff cared and hotel condition) 
+# where there was the most highest LTR.
+df_max_ltr <- LCS[LCS$Likelihood_Recommend_H=max(LCS$Likelihood_Recommend_H),]
+
 
 ## end your R code and logic 
 
