13da018258a5f3688da1ef06015fb9a7e7ef8332
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 22:39:22 -0400

    saving highest_ltr_cs.R

--- highest_ltr_cs.R
+++ highest_ltr_cs.R
@@ -14,6 +14,7 @@ df <- read.csv('LCS.csv') #The latest cleaned dataframe
 #	PART F.2: Output the ratings data frame with rows where there was the highest ltr.
 keep <- df$Likelihood_Recommend_H == max(df$Likelihood_Recommend_H)
 new_df <- df[keep,]
+head(new_df)
 write.csv(new_df, 'new_df.csv')
 # LTR <- df %>% filter(!is.na(Condition_Hotel_H) & !is.na(Staff_Cared_H)) %>% select(Likelihood_Recommend_H, Condition_Hotel_H, Staff_Cared_H)
 # 
