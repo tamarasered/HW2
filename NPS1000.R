7dcba2d5427e8e12aaf8febd7f0d1c265319554c
tamarasered <39306870+tamarasered@users.noreply.github.com>
2018-05-15 21:53:43 -0400

    saving NPS1000.R

--- NPS1000.R
+++ NPS1000.R
@@ -10,6 +10,26 @@ df <- read.csv('LCS.csv')
 
 ## start writing your R code from here
 
+#Sampling 100 surveys and assingning as vector LTR
+LTR <- sample(df$Likelihood_Recommend_H, 1000, replace=FALSE)
+
+#Running the function NPS
+NPS <- function(LTR) {
+  promoters  <- (sum(LTR > 8))/length(LTR)
+  detractors <- (sum(LTR < 7))/length(LTR)
+  nps <- promoters-detractors 
+  return(nps)
+}
+#Checking the return from the function
+NPS(LTR)
+
+#Running the mean value of each sample 1000 times
+AVG <- replicate(100, mean(sample(df$Likelihood_Recommend_H, 1000, replace=FALSE)))
+
+#Creating a histogram of the mean values
+png(filename="hist_NPS1000.png")
+hist(AVG)
+dev.off()
 ## end your R code and logic 
 
 ####################################
